1dc0b7116bbced9784234b3d9f28cd6e383f4f42
paidaxing <32479732@qq.com>
2024-01-06 23:40:41 +0800

    update chatapi for web

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -45,7 +45,7 @@ export function fetchChatAPIProcess<T = any>(
   }
 
   return post<T>({
-    url: '/chat-process',
+    url: '/chat/chat-process',
     data,
     signal: params.signal,
     onDownloadProgress: params.onDownloadProgress,
