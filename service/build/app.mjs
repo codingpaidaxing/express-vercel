5aa090444b8e0aeef730cb1b3983a813a7323bac
paidaxing <32479732@qq.com>
2024-01-06 23:43:51 +0800

    build

--- service/build/app.mjs
+++ service/build/app.mjs
@@ -1,5 +1,5 @@
 // src/app.ts
-import express2 from "express";
+import express3 from "express";
 
 // src/routes/users.ts
 import express from "express";
@@ -88,11 +88,196 @@ router.get("/wx-auth", function(req, res, next) {
 });
 var users_default = router;
 
-// src/app.ts
-var app = express2();
+// src/routes/chatgpt/index.ts
+import express2 from "express";
+
+// src/chatgpt/index.ts
+import * as dotenv from "dotenv";
+import "isomorphic-fetch";
+import { ChatGPTAPI, ChatGPTUnofficialProxyAPI } from "chatgpt";
+import { SocksProxyAgent } from "socks-proxy-agent";
+import httpsProxyAgent from "https-proxy-agent";
+import fetch from "node-fetch";
+
+// src/utils/is.ts
+function isNotEmptyString(value) {
+  return typeof value === "string" && value.length > 0;
+}
+
+// src/chatgpt/index.ts
+var { HttpsProxyAgent } = httpsProxyAgent;
+dotenv.config();
+var ErrorCodeMessage = {
+  401: "[OpenAI] \u63D0\u4F9B\u9519\u8BEF\u7684API\u5BC6\u94A5 | Incorrect API key provided",
+  403: "[OpenAI] \u670D\u52A1\u5668\u62D2\u7EDD\u8BBF\u95EE\uFF0C\u8BF7\u7A0D\u540E\u518D\u8BD5 | Server refused to access, please try again later",
+  502: "[OpenAI] \u9519\u8BEF\u7684\u7F51\u5173 |  Bad Gateway",
+  503: "[OpenAI] \u670D\u52A1\u5668\u7E41\u5FD9\uFF0C\u8BF7\u7A0D\u540E\u518D\u8BD5 | Server is busy, please try again later",
+  504: "[OpenAI] \u7F51\u5173\u8D85\u65F6 | Gateway Time-out",
+  500: "[OpenAI] \u670D\u52A1\u5668\u7E41\u5FD9\uFF0C\u8BF7\u7A0D\u540E\u518D\u8BD5 | Internal Server Error"
+};
+var timeoutMs = !isNaN(+process.env.TIMEOUT_MS) ? +process.env.TIMEOUT_MS : 100 * 1e3;
+var disableDebug = process.env.OPENAI_API_DISABLE_DEBUG === "true";
+var apiModel;
+var model = isNotEmptyString(process.env.OPENAI_API_MODEL) ? process.env.OPENAI_API_MODEL : "gpt-3.5-turbo";
+if (!isNotEmptyString(process.env.OPENAI_API_KEY) && !isNotEmptyString(process.env.OPENAI_ACCESS_TOKEN))
+  throw new Error("Missing OPENAI_API_KEY or OPENAI_ACCESS_TOKEN environment variable");
+var api;
+(async () => {
+  if (isNotEmptyString(process.env.OPENAI_API_KEY)) {
+    const OPENAI_API_BASE_URL = process.env.OPENAI_API_BASE_URL;
+    const options = {
+      apiKey: process.env.OPENAI_API_KEY,
+      completionParams: { model },
+      debug: !disableDebug
+    };
+    if (model.toLowerCase().includes("gpt-4")) {
+      if (model.toLowerCase().includes("32k")) {
+        options.maxModelTokens = 32768;
+        options.maxResponseTokens = 8192;
+      } else {
+        options.maxModelTokens = 8192;
+        options.maxResponseTokens = 2048;
+      }
+    }
+    if (isNotEmptyString(OPENAI_API_BASE_URL))
+      options.apiBaseUrl = `${OPENAI_API_BASE_URL}/v1`;
+    setupProxy(options);
+    api = new ChatGPTAPI({ ...options });
+    apiModel = "ChatGPTAPI";
+  } else {
+    const options = {
+      accessToken: process.env.OPENAI_ACCESS_TOKEN,
+      apiReverseProxyUrl: isNotEmptyString(process.env.API_REVERSE_PROXY) ? process.env.API_REVERSE_PROXY : "https://ai.fakeopen.com/api/conversation",
+      model,
+      debug: !disableDebug
+    };
+    setupProxy(options);
+    api = new ChatGPTUnofficialProxyAPI({ ...options });
+    apiModel = "ChatGPTUnofficialProxyAPI";
+  }
+})();
+async function chatReplyProcess(options) {
+  const { message, lastContext, process: process2, systemMessage, temperature, top_p } = options;
+  try {
+    let options2 = { timeoutMs };
+    if (apiModel === "ChatGPTAPI") {
+      if (isNotEmptyString(systemMessage))
+        options2.systemMessage = systemMessage;
+      options2.completionParams = { model, temperature, top_p };
+    }
+    if (lastContext != null) {
+      if (apiModel === "ChatGPTAPI")
+        options2.parentMessageId = lastContext.parentMessageId;
+      else
+        options2 = { ...lastContext };
+    }
+    const response = await api.sendMessage(message, {
+      ...options2,
+      onProgress: (partialResponse) => {
+        process2?.(partialResponse);
+      }
+    });
+    return sendResponse({ type: "Success", data: response });
+  } catch (error) {
+    const code = error.statusCode;
+    global.console.log(error);
+    if (Reflect.has(ErrorCodeMessage, code))
+      return sendResponse({ type: "Fail", message: ErrorCodeMessage[code] });
+    return sendResponse({ type: "Fail", message: error.message ?? "Please check the back-end console" });
+  }
+}
+function setupProxy(options) {
+  if (isNotEmptyString(process.env.SOCKS_PROXY_HOST) && isNotEmptyString(process.env.SOCKS_PROXY_PORT)) {
+    const agent = new SocksProxyAgent({
+      hostname: process.env.SOCKS_PROXY_HOST,
+      port: process.env.SOCKS_PROXY_PORT,
+      userId: isNotEmptyString(process.env.SOCKS_PROXY_USERNAME) ? process.env.SOCKS_PROXY_USERNAME : void 0,
+      password: isNotEmptyString(process.env.SOCKS_PROXY_PASSWORD) ? process.env.SOCKS_PROXY_PASSWORD : void 0
+    });
+    options.fetch = (url, options2) => {
+      return fetch(url, { agent, ...options2 });
+    };
+  } else if (isNotEmptyString(process.env.HTTPS_PROXY) || isNotEmptyString(process.env.ALL_PROXY)) {
+    const httpsProxy = process.env.HTTPS_PROXY || process.env.ALL_PROXY;
+    if (httpsProxy) {
+      const agent = new HttpsProxyAgent(httpsProxy);
+      options.fetch = (url, options2) => {
+        return fetch(url, { agent, ...options2 });
+      };
+    }
+  } else {
+    options.fetch = (url, options2) => {
+      return fetch(url, { ...options2 });
+    };
+  }
+}
+
+// src/chatgpt/middleware/auth.ts
+var auth = async (req, res, next) => {
+  const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY;
+  if (isNotEmptyString(AUTH_SECRET_KEY)) {
+    try {
+      const Authorization = req.header("Authorization");
+      if (!Authorization || Authorization.replace("Bearer ", "").trim() !== AUTH_SECRET_KEY.trim())
+        throw new Error("Error: \u65E0\u8BBF\u95EE\u6743\u9650 | No access rights");
+      next();
+    } catch (error) {
+      res.send({ status: "Unauthorized", message: error.message ?? "Please authenticate.", data: null });
+    }
+  } else {
+    next();
+  }
+};
+
+// src/chatgpt/middleware/limiter.ts
+import { rateLimit } from "express-rate-limit";
+var MAX_REQUEST_PER_HOUR = process.env.MAX_REQUEST_PER_HOUR;
+var maxCount = isNotEmptyString(MAX_REQUEST_PER_HOUR) && !isNaN(Number(MAX_REQUEST_PER_HOUR)) ? parseInt(MAX_REQUEST_PER_HOUR) : 0;
+var limiter = rateLimit({
+  windowMs: 60 * 60 * 1e3,
+  // Maximum number of accesses within an hour
+  max: maxCount,
+  statusCode: 200,
+  // 200 means success，but the message is 'Too many request from this IP in 1 hour'
+  message: async (req, res) => {
+    res.send({ status: "Fail", message: "Too many request from this IP in 1 hour", data: null });
+  }
+});
+
+// src/routes/chatgpt/index.ts
 var router2 = express2.Router();
-var port = 3002;
+router2.post("/chat-process", [auth, limiter], async (req, res) => {
+  res.setHeader("Content-type", "application/octet-stream");
+  try {
+    const { prompt, options = {}, systemMessage, temperature, top_p } = req.body;
+    let firstChunk = true;
+    await chatReplyProcess({
+      message: prompt,
+      lastContext: options,
+      process: (chat) => {
+        res.write(firstChunk ? JSON.stringify(chat) : `
+${JSON.stringify(chat)}`);
+        firstChunk = false;
+      },
+      systemMessage,
+      temperature,
+      top_p
+    });
+  } catch (error) {
+    res.write(JSON.stringify(error));
+  } finally {
+    res.end();
+  }
+});
+var chatgpt_default = router2;
+
+// src/app.ts
+var app = express3();
+app.use(express3.json());
+var router3 = express3.Router();
+var port = 3003;
 app.use("/users", users_default);
+app.use("/chat", chatgpt_default);
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript Express!");
 });
