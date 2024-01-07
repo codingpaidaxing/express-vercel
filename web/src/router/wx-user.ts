5fcc941dc2e45cbe942c6dd3ff09a041d21bd975
paidaxing <32479732@qq.com>
2024-01-07 16:38:55 +0800

    add log for url

--- web/src/router/wx-user.ts
+++ web/src/router/wx-user.ts
@@ -2,6 +2,18 @@ import type { Router } from 'vue-router'
 import { wxUseAuthStoreWithout } from '@/store/modules/auth/wx-user'
 
 export function setupWxUserInfo(router: Router) {
+    // 获取当前路由信息
+    const currentRoute = router.currentRoute.value
+
+    // 获取完整的 URL 路径
+    const fullPath = currentRoute.fullPath
+
+    console.log(fullPath)
+    // 获取路由参数
+    const routeParams = currentRoute.params
+
+    console.log(routeParams)
+
     console.log('router', router)
     router.beforeEach(async (to, from, next) => {
         const wxUserAuthStore = wxUseAuthStoreWithout()
@@ -14,7 +26,7 @@ export function setupWxUserInfo(router: Router) {
                 if (to.path === '/500')
                     next({ name: 'Root' })
                 else
-                // console.log(wxUserAuthStore.wx_token)
+                    // console.log(wxUserAuthStore.wx_token)
                     next()
             }
             catch (error) {
